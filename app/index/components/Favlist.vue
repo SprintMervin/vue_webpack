900bd0b962995ddb5d1d9f4b58872a26f56cd28f
sprintmervin <jssch@foxmail.com>
2016-08-29 17:51:39 +0800

    create repository

---
+++ app/index/components/Favlist.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>test page</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+    html{
+        background: #ccc;
+    }
+</style>
---
+++ app/index/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by HUI on 16/8/29.
+ */
+import Vue from 'Vue'
+import Favlist from './components/Favlist'
+
+new Vue({
+    el: 'body',
+    components: { Favlist }
+})
